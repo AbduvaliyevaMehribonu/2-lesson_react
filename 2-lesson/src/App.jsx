7bac257cfa9e03481ae41f8ec7a2b31f37885c2c
AbduvaliyevaMehribonu <abduvaliyevamehribon@gmail.com>
2023-08-21 15:55:09 +0500

    chart 2 added

--- 2-lesson/src/App.jsx
+++ 2-lesson/src/App.jsx
@@ -1,5 +1,6 @@
 import './app.scss'
 import Chart1 from './components/charts/chart1';
+import Chart2 from './components/charts/chart2';
 import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
 
@@ -12,7 +13,10 @@ function App() {
   <Sidebar/>
 <div className="navbar-charts-wrapper">
 <Navbar/>
+<div className="charts-wrapper">
 <Chart1/>
+<Chart2/>
+</div>
 </div>
  </div>
   )
---
+++ 2-lesson/src/components/charts/Chart2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import chart2 from '../../assets/images/chart2.png'
+
+const Chart2 = () => {
+  return (
+    <div className="chart2-wrapper">
+        <p>Oylik maosh va to'lovlar</p>
+        <img src={chart2} alt="" />
+    </div>
+  )
+}
+
+export default Chart2
